f9bc4106c167172d4ec22b8b686a07e63a4a2565
TheOneCoderGod <ubasenete7@gmail.com>
2022-07-13 21:56:29 +0800

    Updates

---
+++ precache-manifest.375f4b707f7f488496931ac05e10c9d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca5ea6435fbbd7b71149",
+    "url": "/robofriends/static/css/main.6652c52d.chunk.css"
+  },
+  {
+    "revision": "ca5ea6435fbbd7b71149",
+    "url": "/robofriends/static/js/main.674b4884.chunk.js"
+  },
+  {
+    "revision": "75dcfc39fa8879b648fd",
+    "url": "/robofriends/static/js/runtime~main.e6791cfe.js"
+  },
+  {
+    "revision": "9253f9fdd509feeb9439",
+    "url": "/robofriends/static/css/2.35a3c3ef.chunk.css"
+  },
+  {
+    "revision": "9253f9fdd509feeb9439",
+    "url": "/robofriends/static/js/2.fd0ca411.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/robofriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a7ec61b1959f86337a1cfe1382669d86",
+    "url": "/robofriends/index.html"
+  }
+];
